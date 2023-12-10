14ff4755c8c32f9c2a399e79ecb72d1092bbd1de
unknown <nandakishorekallarackal@gmail.com>
2023-12-11 02:44:07 +0530

    speakers added

--- application/views/speakers.php
+++ application/views/speakers.php
@@ -421,6 +421,64 @@
 
 
 <style>
+  body {
+  margin: 0;
+  padding: 0;
+  font-family: Arial, sans-serif;
+  background-color: #f8f9fa;
+}
+
+.speaker-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  margin: 20px auto;
+}
+
+.speaker-card {
+  text-align: center;
+  margin: 15px;
+  padding: 15px;
+  background-color: #ffffff;
+  border-radius: 8px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  flex-basis: calc(33.33% - 30px); /* Adjust the width as needed */
+}
+
+.speaker-image {
+  width: 50%;
+  height: 15em;
+  margin : 5px;
+  border-radius: 8px;
+}
+
+.speaker-details {
+  margin-top: 15px;
+  display: flex;
+  flex-direction: column;
+}
+
+.details-container {
+  display : flex;
+  height: 16em; /* Set a fixed height */
+
+  width: 100%;
+  margin: 10px;
+  padding: 5px;
+  background-color: #f0f0f0;
+  border-radius: 8px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+.description {
+  margin-top: 25px;
+  text-align: justify;
+  clear: both; /* Add this property to ensure the description is below the details */
+}
+
+
+ 
+
 div.gallery {
   border: 1px solid #ccc;
 }
@@ -471,8 +529,148 @@ div.desc {
 </head>
 <body>
 
+<div class="speaker-container">
+<div class="speaker-card">
+      <div class="details-container">
+      <img src="<?php echo $this->config->item('base_url'); ?>assets/images/speakers/speaker-2024-1.jpg" alt="Speaker 1" class="speaker-image">
+      
+      <div class="speaker-details">
+        
+        <h3>Dr George Palattiyil, PhD, SFHEA</h3>
+        <h4>Head of Social Work & Senior Lecturer</h4>
+        <h5>School of Social and Political Science, The University of Edinburgh</h5>
+      </div>
+</div>
+      <p class="description">
+      Dr. Palattiyil has been a member of staff at the University
+since 2009, director of the successful and internationally
+regarded Master of Social Work programme in recent
+years and is a Senior Fellow of the Higher Education
+Academy.
+With teaching and research experience spanning
+across India and Scotland, he is passionate about
+internationalising the curricula and enabling students to have cross-cultural
+experiences and is deeply committed to diversity and social justice. His
+teaching and research interests are in the area of forced migration and
+refugees, human rights, individuals and families affected by HIV and AIDS
+and older people. He is a member of the Board of Directors of Multi-Cultural
+Family Base, a third-sector organisation in Edinburgh supporting BAME
+children and families while also providing mentoring support to BAME social
+work students. For his contribution to student support, he was awarded
+the Best Personal Tutor Award (2019) by the Edinburgh University Students
+Association.</p>
+</div>
 
-<div style="  border-style: solid;
+<div class="speaker-card">
+      <div class="details-container">
+      <img src="<?php echo $this->config->item('base_url'); ?>assets/images/speakers/speaker-2024-2.jpg" alt="Speaker 1" class="speaker-image">
+      
+      <div class="speaker-details">
+     
+
+        <h3> Dr. Varoshini Nadesan </h3>
+        <h4>Senior Lecturer of the Department of Social Work and Community Development
+</h4>
+        <h5>University of Johannesburg</h5>
+      </div>
+</div>
+      <p class="description">Dr. Varoshini Nadesan currently serves as a senior lecturer in the Department of Social Work and Community Development at the University of Johannesburg. Dr. Varoshini Nadesan is the former Treasurer and President of the Association of South African Social Work Education Institutions. She is currently a consultant on the IASSW Global Standards (GS) and appointed the Convenor of the IASSW Task Force for the Global Standards for Social Work Education to develop the IASSW GS online learning programme. She conducts research in social work education, field instruction, alcoholism child and youth care work, and restorative justice. 
+</p>
+</div>
+
+<div class="speaker-card">
+      <div class="details-container">
+      <img src="<?php echo $this->config->item('base_url'); ?>assets/images/speakers/speaker-2024-3.jpg" alt="Speaker 1" class="speaker-image">
+      
+      <div class="speaker-details">
+        
+        <h3>Dr Rimple Mehta</h3>
+        <h4>Senior Lecturer in Social Work | Parramatta South Campus </h4>
+        <h5>Associate Dean- International (South and Southeast Asia)
+School of Social Sciences
+Western Sydney University, Australia
+</h5>
+      </div>
+</div>
+      <p class="description">
+      Dr. Rimple Mehta is Associate Dean of international (South Asia and South East Asia), and Lecturer of social Work and Community Welfare, at the School of Social Sciences, Western Sydney University. Her research and field engagements broadly focus on women prisoners, refugee women, and trafficking. She also engages with questions of borders, citizenship and the criminology of mobility. She adopts an intersectional feminist perspective with a strong emphasis on questions of epistemology, therefore viewing ‘reality’ from intersecting positions of marginality.  
+Previously she was an Assistant Professor at the Tata Institute for Social Sciences, Mumbai and an Assistant Professor at the School of Women’s Studies, Jadavpur University. She studied Sociology, Social Work and Women’s Studies and has written on gender, borders, sexuality and prisons, especially the criminalization of mobility and Bangladeshi women in Indian prisons. Her paper titled "So Many Ways to Love You/Self: Negotiating Love in a Prison'' won the 2013 Enloe Award. She has worked with organisations such as Swayam and networks such as Maitree against violence on women in West Bengal, as well as with women prisoners in Mumbai, Kolkata and the Netherlands.
+
+</p>
+</div>
+
+<div class="speaker-card">
+      <div class="details-container">
+      <img src="<?php echo $this->config->item('base_url'); ?>assets/images/speakers/speaker-2024-4.jpg" alt="Speaker 1" class="speaker-image">
+      
+      <div class="speaker-details">
+        
+        <h3>Prof. Dr. Stephanie Goeke </h3>
+        <!-- <h4></h4>
+        <h5></h5> -->
+      </div>
+</div>
+      <p class="description">
+      Professor of theory and practice of social work/practical research in social work.
+Prof.Dr. Stephanie Goeke currently serves as the professor of theory and practice of social work/practica; research in social work at the Evangelical University of  Ludwigsburg Protestant University of Applied Sciences. She worked as a consultant in the competence centre for social policy and social work at Diözesan Caritasverband Rottenburg-Stuttgart e.V. Additionally, she was an honorary teacher for the subject of social management at the private vocational school Dr Engel in Böblingen and Stuttgart. She also serves as a research assistant at the Evangelische Hochschule Ludwigsburg, providing scientific support for the "Living in Place" project. Furthermore, Prof.Stephanie worked as a research assistant at the Pedagogical Institute, under the guidance of Prof. Dr. E. Heinemann at Johannes Gutenberg University in Mainz.
+
+</p>
+</div>
+
+<div class="speaker-card">
+      <div class="details-container">
+      <img src="<?php echo $this->config->item('base_url'); ?>assets/images/speakers/speaker-2024-5.jpg" alt="Speaker 1" class="speaker-image" style = "height : 12em;">
+      
+      <div class="speaker-details">
+        
+        <h3>Prof. Dr. Sandra Fietkau</h3>
+        <h4>University Disability Officer</h4>
+        <h5>Head of the Inclusive Pedagogy and Special Education program</h5>
+        <h5>Member of the scholarship award committee of the EH Foundation</h5>
+      </div>
+</div>
+      <p class="description">
+      Dr. Sandra Fietkau currently works as head of the inclusive pedagogy and special education program at the Evangelical University of Ludwigsburg, Protestant University of Applied Sciences. She started working with people with disabilities while she was still at school. After studying social work (social work/pedagogy, FH), she worked in various areas of offerings and services for people with disabilities, led various projects on the topic of inclusion and has been a professor with a focus on inclusion at the Evangelische Hochschule Ludwigsburg since 2018. Since February 2023, external lecturer at the Bertha von Suttner Private University of St. Pölten. She also works as a moderator for personal future planning, as a coach and supervisor and as a moderator. 
+</p>
+</div>
+
+<div class="speaker-card">
+      <div class="details-container">
+      <img src="<?php echo $this->config->item('base_url'); ?>assets/images/speakers/speaker-2024-6.jpg" alt="Speaker 1" class="speaker-image">
+      
+      <div class="speaker-details">
+        
+        <h3>Dr. Ina Schildbach</h3>
+        <h4>Professor of political science with a focus on social policy</h4>
+        <h5>Faculty of Applied Social and Health Sciences,
+OTH Regensburg, Germany</h5>
+      </div>
+</div>
+      <p class="description">
+      Dr. Ina Schildbach teaches political science at the Friedrich-Alexander University Erlangen-Nuremberg and at the Georg Simon Ohm Technical University Nuremberg. Her research focuses on poverty, migration and racism as well as political economy. She has been a political science professor at OTH Regensburg and lectures at Nuremberg Technical University and the Evangelical University of Nuremberg. Her focus is on poverty, social policy, political economy, right-wing populism, flight and migration, and political education in the post-factual age.  Since 2014, she conducted various political science lectures, workshops, and discussions as part of social initiatives, church associations, and adult education centres, including "Rights instead of Leftovers".
+</p>
+</div>
+  
+<div class="speaker-card">
+      <div class="details-container">
+      <img src="<?php echo $this->config->item('base_url'); ?>assets/images/speakers/speaker-2024-7.jpg" alt="Speaker 1" class="speaker-image">
+      
+      <div class="speaker-details">
+        
+        <h3>Dr.Derek M. Doroski</h3>
+        <h4>Assistant Professor of Biology</h4>
+        <h5>1235 Franciscan University of Steubenville, USA</h5>
+      </div>
+</div>
+      <p class="description">
+      Dr. Doroski currently serves as a professor of biology and the director of the engineering program. He received his doctorate in biomedical engineering from the Georgia Institute of Technology and currently teaches freshman biology, developmental biology, and rigid body dynamics. Dr. Doroski’s experimental research is focused on stem cell delivery and tissue engineering and has been published in scientific journals such as Biomaterials, Tissue Engineering, and PLOS One. He is a regular speaker on IVF, stem cells and other bioethical topics. Dr. Doroski’s analysis demonstrating how Humanae vitae has advanced reproductive healthcare contributed to the book Why Humanae Vitae Is Still Right, which was edited by Janet Smith. He has also written and spoken on the dominance of adult stem cells in clinical trials, how to understand when organisms begin from a biological perspective, and what the biological data says about the metaphysics of embryo fusion. His work has been presented at conferences on Bioethics and the Philosophy of Medicine and published in journals such as the Linacre Quarterly, the journal of the Catholic Medical Association, and Theoretical Medicine and Bioethics. 
+</p>
+</div>
+
+
+
+  </div>
+<!-- <div style="  border-style: solid;
   border-color: #ff0000; ">
 <div class="responsive">
   <div class="gallery">
@@ -509,7 +707,7 @@ div.desc {
     </a>
 
   </div>
-</div>
+</div> -->
 <!--<div class="responsive">-->
 <!--  <div class="gallery">-->
 <!--    <a target="_blank" href="<?php echo $this->config->item('base_url')."assets/"; ?>images/banner1/5.png" >-->
@@ -519,14 +717,14 @@ div.desc {
 <!--  </div>-->
 <!--</div>-->
 
-<div class="responsive">
+<!-- <div class="responsive">
   <div class="gallery">
     <a target="_blank" href="<?php echo $this->config->item('base_url')."assets/"; ?>images/banner1/6.png" >
       <img src="<?php echo $this->config->item('base_url')."assets/"; ?>images/banner1/6.png" alt="Cinque Terre" width="600" height="400">
     </a>
 
   </div>
-</div>
+</div> -->
 
 <!--<div class="responsive">-->
 <!--  <div class="gallery">-->
@@ -555,7 +753,7 @@ div.desc {
 <!--  </div>-->
 <!--</div>-->
 
-<div class="responsive">
+<!-- <div class="responsive">
   <div class="gallery">
     <a target="_blank" href="<?php echo $this->config->item('base_url')."assets/"; ?>images/banner1/10.png" >
       <img src="<?php echo $this->config->item('base_url')."assets/"; ?>images/banner1/10.png" alt="Cinque Terre" width="600" height="400">
@@ -616,11 +814,11 @@ div.desc {
     </a>
 
   </div>
-</div>
+</div> -->
 
 
 
-</div>
+<!-- </div> -->
 
 <div class="clearfix"></div>
 
